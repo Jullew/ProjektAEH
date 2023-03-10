b4facd680e4145796cb22e247566544ce0500657
Julian <jullewdevpl@gmail.com>
2023-03-10 21:28:12 +0100

    first commit

---
+++ src/main/java/com/projektaeh/systemaukcji/SystemaukcjiApplication.java
@@ -0,0 +1,13 @@
+package com.projektaeh.systemaukcji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemaukcjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemaukcjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projektaeh/systemaukcji/SystemaukcjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projektaeh.systemaukcji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemaukcjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
